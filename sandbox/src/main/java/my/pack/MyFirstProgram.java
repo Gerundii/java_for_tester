caa18d733434948fd6fcfcfda5c6ef858eca8556
Mikhail Tekuchev <m.tekuchev@gmail.com>
2023-12-06 00:23:50 +0300

    add package

--- sandbox/src/main/java/my/pack/MyFirstProgram.java
+++ sandbox/src/main/java/my/pack/MyFirstProgram.java
@@ -1,6 +1,8 @@
+package my.pack;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow, world!");
+		System.out.println("Hello, world!");
 	}
 }
