f7ab1514d989f0fcf134df29d21490b9f97f7205
Mikhail Tekuchev <m.tekuchev@gmail.com>
2023-12-06 10:06:26 +0300

    add area function

--- sandbox/src/main/java/my/pack/MyFirstProgram.java
+++ sandbox/src/main/java/my/pack/MyFirstProgram.java
@@ -1,8 +1,27 @@
 package my.pack;
 
 public class MyFirstProgram {
-
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("Vasya");
+		System.out.println();
+
+		double l = 25.0;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 8.0;
+		double b = 7.0;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+	public static void hello (String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double l) {
+		return l * l;
+	}
+
+	public static double area (double a, double b) {
+		return a * b;
 	}
 }
