20e29e7756ef73d3d8639651e9aa81e2399955b1
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-15 18:57:27 +0300

    fix EquationTests class
    Добавил тесты для условий, когда a = 0, b = 0, c = 0

--- sandbox/src/test/java/my/pack/addressbook/EquationTests.java
+++ sandbox/src/test/java/my/pack/addressbook/EquationTests.java
@@ -7,20 +7,38 @@ import org.testng.annotations.Test;
 public class EquationTests {
 
     @Test (description = "Квадратное уравнение не имеет корней, дискриминант меньше нуля")
-    public void test0() {
+    public void testDiscriminantLessThanZero() {
         Equation e = new Equation(1, 1, 1);
         Assert.assertEquals(e.rootNubmer(), 0);
     }
 
     @Test (description = "Квадратное уравнение имеет один корень, дискриминант равен нулю")
-    public void test1() {
+    public void testDiscriminantEqualsZero() {
         Equation e = new Equation(1, 2, 1);
         Assert.assertEquals(e.rootNubmer(), 1);
     }
 
     @Test (description = "Квадратное уравнение имеет два корня, дискриминант больше нуля")
-    public void test2() {
+    public void testDiscriminantGraterThanZero() {
         Equation e = new Equation(1, 5, 6);
         Assert.assertEquals(e.rootNubmer(), 2);
+    }
+
+    @Test (description = "Линейное уравнение")
+    public void testLinearEquation() {
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(e.rootNubmer(), 1);
+    }
+
+    @Test (description = "Константа")
+    public void testConstant() {
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(e.rootNubmer(), 0);
+    }
+
+    @Test (description = "0 = 0")
+    public void testZero() {
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(e.rootNubmer(), -1);
     }
 }
