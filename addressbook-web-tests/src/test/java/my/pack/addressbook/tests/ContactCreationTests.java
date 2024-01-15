2db4e8d604b0574116025e73f0ea7411db4b7dd2
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-15 14:57:11 +0300

    fix ContactCreationTest
    Добавил цикл для создания нескольких контактов

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import my.pack.addressbook.model.ContactData;
 import org.testng.annotations.*;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class ContactCreationTests extends TestBase{
 
@@ -12,13 +13,16 @@ public class ContactCreationTests extends TestBase{
   public void testContactCreation() throws Exception {
     ContactData nibbler = new ContactData("Nibbler", "", "", "Nibbler", "Lord", "", "nibbler@futurama.com", "", "1", "August", "2000");
     ContactData fry = new ContactData("Phillip", "J", "Fry", "Fry", "Delivery Guy", "+982-265-265-22", "fry@futurama.com", "", "31", "December", "1999");
-    app.getNavigationHelper().gotoContactCreatePage();
-    app.getContactHelper().fillContactForm(nibbler);
-    app.getContactHelper().submitContactCreation();
-    app.getContactHelper().returnToHomePage();
-    app.getNavigationHelper().gotoContactCreatePage();
-    app.getContactHelper().fillContactForm(fry);
-    app.getContactHelper().submitContactCreation();
-    app.getContactHelper().returnToHomePage();
+    ContactData unknown = new ContactData("Unknown", "", "Man", "Unknown", "Somebody", "", "unknown@man.su", "", "13", "December", "1000");
+    ArrayList<ContactData> contacts = new ArrayList<>();
+    contacts.add(nibbler);
+    contacts.add(fry);
+    contacts.add(unknown);
+    for (ContactData contact: contacts) {
+      app.getNavigationHelper().gotoContactCreatePage();
+      app.getContactHelper().fillContactForm(contact);
+      app.getContactHelper().submitContactCreation();
+      app.getContactHelper().returnToHomePage();
+    }
   }
 }
