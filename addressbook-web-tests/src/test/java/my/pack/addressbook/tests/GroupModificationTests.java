42aaa103d4eccb10bda3481191ae227a874cc992
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-15 16:18:45 +0300

    add GroupModificationTests class
    Добавил GroupModificationTests в котором описал тест модификации группы

---
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,15 @@
+package my.pack.addressbook.tests;
+
+import my.pack.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase{
+    @Test
+    public void testGroupModification () {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("mod1", "mod2", "mod3"));
+        app.getGroupHelper().submitGroupModification();
+    }
+}
