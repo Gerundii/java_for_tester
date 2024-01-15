43203de25c252f8d2899d7220b92c6150c42b396
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-15 19:55:26 +0300

    fix ApplicationManager class
    Добавил в класс ApplicationManager:
    - поле Browser
    - конструктор с параметром Browser
    
    В метод init класс ApplicationManager добавил условие для выбора запускаемого браузера в зависимости от параметра, переданного на вход при создании объекта класса ApplicationManager в классе TestBase

--- addressbook-web-tests/src/test/java/my/pack/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/appmanager/ApplicationManager.java
@@ -1,12 +1,18 @@
 package my.pack.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.time.Duration;
 
 public class ApplicationManager {
 
+    private Browser browser;
     public WebDriver wd;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
@@ -14,9 +20,19 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     public JavascriptExecutor js;
 
+    public ApplicationManager(Browser browser) {
+        this.browser = browser;
+    }
+
     public void init() {
+        if (browser == Browser.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == Browser.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == Browser.EDGE) {
+            wd = new EdgeDriver();
+        }
         //System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
-        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         js = (JavascriptExecutor) wd;
         wd.get("http://localhost:8080/addressbook/");
--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package my.pack.addressbook.tests;
 
 import my.pack.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(Browser.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
