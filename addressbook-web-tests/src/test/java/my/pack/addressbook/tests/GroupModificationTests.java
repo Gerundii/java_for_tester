49c3eaf67aba31b1d1d6e5c1790580bc132ad52a
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-15 16:20:45 +0300

    fix GroupModificationTests
    добавил в тест вызов метода returnToGroupPage

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupModificationTests.java
@@ -11,5 +11,6 @@ public class GroupModificationTests extends TestBase{
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("mod1", "mod2", "mod3"));
         app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
     }
 }
