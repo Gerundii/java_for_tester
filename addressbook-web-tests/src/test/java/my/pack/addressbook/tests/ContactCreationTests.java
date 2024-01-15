62918948a4629b87bf4dc1f9fe41afaa487cb037
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-15 15:39:23 +0300

    fix ContactCreationTests class
    Вынес создание Тестовых данных из тестового метого

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactCreationTests.java
@@ -8,16 +8,18 @@ import java.util.ArrayList;
 
 public class ContactCreationTests extends TestBase{
 
+  ContactData nibbler = new ContactData("Nibbler", "", "", "Nibbler", "Lord", "", "nibbler@futurama.com", "", "1", "August", "2000");
+  ContactData fry = new ContactData("Phillip", "J", "Fry", "Fry", "Delivery Guy", "+982-265-265-22", "fry@futurama.com", "", "31", "December", "1999");
+  ContactData unknown = new ContactData("Unknown", "", "Man", "Unknown", "Somebody", "", "unknown@man.su", "", "13", "December", "1000");
+  ArrayList<ContactData> contacts = new ArrayList<>();
 
   @Test
   public void testContactCreation() throws Exception {
-    ContactData nibbler = new ContactData("Nibbler", "", "", "Nibbler", "Lord", "", "nibbler@futurama.com", "", "1", "August", "2000");
-    ContactData fry = new ContactData("Phillip", "J", "Fry", "Fry", "Delivery Guy", "+982-265-265-22", "fry@futurama.com", "", "31", "December", "1999");
-    ContactData unknown = new ContactData("Unknown", "", "Man", "Unknown", "Somebody", "", "unknown@man.su", "", "13", "December", "1000");
-    ArrayList<ContactData> contacts = new ArrayList<>();
+
     contacts.add(nibbler);
     contacts.add(fry);
     contacts.add(unknown);
+
     for (ContactData contact: contacts) {
       app.getNavigationHelper().gotoContactCreatePage();
       app.getContactHelper().fillContactForm(contact);
