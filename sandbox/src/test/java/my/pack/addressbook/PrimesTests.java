24f7d65942ee289802eeb78981a7f9fed37d93a6
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-01-31 13:33:45 +0300

    add Collections

--- sandbox/src/test/java/my/pack/addressbook/PrimesTests.java
+++ sandbox/src/test/java/my/pack/addressbook/PrimesTests.java
@@ -9,29 +9,21 @@ public class PrimesTests {
 
     @Test(description = "Число простое")
     public void testNumberIsPrime() {
-        /*boolean isPrime = Primes.isPrime(7);
-        Assert.assertEquals(isPrime, true);*/
         Assert.assertTrue(Primes.isPrime(7));
     }
 
     @Test(description = "Число не простое")
     public void testNumberIsNotPrime() {
-        /*boolean isPrime = Primes.isPrime(8);
-        Assert.assertEquals(isPrime, false);*/
         Assert.assertFalse(Primes.isPrime(9));
     }
 
     @Test(description = "Число простое")
     public void testNumberIsPrimeWhile() {
-        /*boolean isPrime = Primes.isPrime(7);
-        Assert.assertEquals(isPrime, true);*/
         Assert.assertTrue(Primes.isPrimeWhile(7));
     }
 
     @Test(description = "Число не простое")
     public void testNumberIsNotPrimeWhile() {
-        /*boolean isPrime = Primes.isPrime(8);
-        Assert.assertEquals(isPrime, false);*/
         Assert.assertFalse(Primes.isPrimeWhile(9));
     }
 }
