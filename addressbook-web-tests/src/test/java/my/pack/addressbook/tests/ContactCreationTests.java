10b35b977621b3225d9d27a2c6820c981b76ff75
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-02-01 16:14:00 +0300

    fix Contact Tests
    Добавил проверку списков контактов при создании, редактировании и удалении

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import my.pack.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -11,10 +12,24 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() throws Exception {
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getNavigationHelper().gotoContactCreatePage();
-        app.getContactHelper().fillContactForm(ContactData.fry, true);
+
+        ContactData contact = new ContactData("Known", "", "Man", "Unknown", "Somebody", "", "unknown@man.su", "", "13", "December", "1000", null);
+
+        app.getContactHelper().fillContactForm(contact, true);
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (ContactData elem : after) {
+            if (elem.getId() > max) {
+                max = elem.getId();
+            }
+        }
+        contact.setId(max);
+        before.add(contact);
+
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 }
--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import my.pack.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase{
@@ -14,12 +15,20 @@ public class ContactModificationTests extends TestBase{
             app.getNavigationHelper().gotoContactCreatePage();
             app.getContactHelper().createContact(ContactData.nibbler);
         }
-        app.getContactHelper().selectContact(before.size() - 2);
-        app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactForm(ContactData.fry, false);
+        app.getContactHelper().selectContact(before.size() - 3);
+        app.getContactHelper().initContactModification(before.get(before.size() - 3).getId());
+
+        ContactData contact = new ContactData(before.get(before.size() - 3).getId(), "somebody@mail.ru");
+
+        app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
+
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 3);
+        before.add(contact);
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 }
