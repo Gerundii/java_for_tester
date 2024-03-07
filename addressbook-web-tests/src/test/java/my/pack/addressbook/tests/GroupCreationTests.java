07805e60dfa12e67548279f3d8dc894f4299a97e
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-07 16:17:02 +0300

    fix GroupCreationTests
    Добавил Data provider

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,27 @@ import my.pack.addressbook.model.GroupData;
 import my.pack.addressbook.model.Groups;
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
-    @Test(enabled = true)
-    public void testGroupCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withGroupName("test1").withGroupHeader("header1").withGroupFooter("footer1")});
+        list.add(new Object[] {new GroupData().withGroupName("test2").withGroupHeader("header2").withGroupFooter("footer2")});
+        list.add(new Object[] {new GroupData().withGroupName("test3").withGroupHeader("header3").withGroupFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(enabled = true, dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withGroupName("Vivi").withGroupHeader("Kolo").withGroupFooter("Gaga");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
