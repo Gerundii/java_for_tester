9885c2d97f117712a267576989c1580bd75e39e3
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-07 15:54:03 +0300

    fix GroupDataGenerator
    Реализована возможность указания параметров командной строки с именами с помощью библиотеки JCommander

--- addressbook-web-tests/src/test/java/my/pack/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/generators/GroupDataGenerator.java
@@ -1,8 +1,10 @@
 package my.pack.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import my.pack.addressbook.model.GroupData;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,14 +13,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
+    @Parameter (names = "-c", description = "Group count")
+    public int count;
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-
-        List<GroupData> groups = generatorGroups(count);
-        save(groups, file);
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+        /*int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);*/
     }
-    private static List<GroupData> generatorGroups(int count) {
+
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+    }
+
+    private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++) {
             groups.add(new GroupData().withGroupName(String.format("test %s", i))
@@ -28,7 +46,7 @@ public class GroupDataGenerator {
         return groups;
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void save(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
             writer.write(String.format("%s;%s;%s\n", group.getGroupName(), group.getGroupHeader(), group.getGroupFooter()));
