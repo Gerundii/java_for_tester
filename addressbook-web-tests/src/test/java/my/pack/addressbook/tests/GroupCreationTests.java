5f6f0a149b0b971f50e3a1a20a2b854afc14c03b
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-11 12:01:20 +0300

    fix GroupCreationTests
    Добавил возможность выбора провайдера данных - в формате csv и в формате xml

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/GroupCreationTests.java
@@ -17,30 +17,35 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
-        //List<Object[]> list = new ArrayList<Object[]>();
+    public Iterator<Object[]> validGroupsCsv() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new GroupData().withName(split[0] + "csv").withHeader(split[1] + "csv").withFooter(split[2] + "csv")});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validGroupsXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
         String xml = "";
         String line = reader.readLine();
         while (line != null) {
             xml += line;
-            line  = reader.readLine();
+            line = reader.readLine();
         }
         XStream xstream = new XStream();
-        xstream.allowTypes(new Class[] {GroupData.class});
-        //xstream.addPermission(AnyTypePermission.ANY);
+        xstream.allowTypes(new Class[]{GroupData.class});
         xstream.processAnnotations(GroupData.class);
         List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-        return groups.stream().map(g -> new Object[] {g}).collect(Collectors.toList()).iterator();
-        /*while (line != null) {
-            String[] split = line.split(";");
-            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
-            line  = reader.readLine();
-        }
-        return list.iterator();*/
+        return groups.stream().map(g -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(enabled = true, dataProvider = "validGroups")
+    @Test(enabled = true, dataProvider = "validGroupsXml")
     public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
@@ -50,6 +55,7 @@ public class GroupCreationTests extends TestBase {
 
         assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt(o -> o.getId()).max().getAsInt()))));
     }
+
     @Test(enabled = true)
     public void testBadGroupCreation() throws Exception {
         app.goTo().groupPage();
