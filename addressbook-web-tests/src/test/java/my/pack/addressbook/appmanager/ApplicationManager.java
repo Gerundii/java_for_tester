29315c173c6efe200afc21ead9ca1e9ce574a18c
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-14 16:35:34 +0300

    fix ApplicationManager
    Добавил инициализацию dbHelper

--- addressbook-web-tests/src/test/java/my/pack/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     public JavascriptExecutor js;
     private Properties properties;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -34,6 +35,9 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
+
         if (browser.equals(Browser.CHROME.browserName())) {
             wd = new ChromeDriver();
         } else if (browser.equals(Browser.FIREFOX.browserName())) {
@@ -72,5 +76,8 @@ public class ApplicationManager {
 
     public ContactHelper contact() {
         return contactHelper;
+    }
+    public DbHelper db() {
+        return dbHelper;
     }
 }
