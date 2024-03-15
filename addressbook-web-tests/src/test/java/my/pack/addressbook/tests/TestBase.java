b0749871a3be55111906ecdd90e50cbb99ddce20
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-15 16:23:42 +0300

    fix TestBase
    Добавил отключаемый метод проверки в интерфейсе verifyGroupListInUI

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/TestBase.java
@@ -1,6 +1,10 @@
 package my.pack.addressbook.tests;
 
 import my.pack.addressbook.appmanager.ApplicationManager;
+import my.pack.addressbook.model.GroupData;
+import my.pack.addressbook.model.Groups;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,10 @@ import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -39,4 +47,13 @@ public class TestBase {
         logger.info("Stop test " + m.getName());
     }
 
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map(group -> new GroupData().withId(group.getId()).withName(group.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
 }
