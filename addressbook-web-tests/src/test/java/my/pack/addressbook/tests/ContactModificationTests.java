2988edc384511a7ec03903f124f7e23fb54ee804
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-15 11:51:03 +0300

    fix ContactModificationTests
    Изменил тесты модификации контактов с использованием БД

--- addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/my/pack/addressbook/tests/ContactModificationTests.java
@@ -18,21 +18,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactModificationTests extends TestBase{
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().homePage();
-        if (app.contact().all().size() == 0) {
+        if (app.db().contacts().size() == 0) {
             app.goTo().contactCreatePage();
-            app.contact().create(new ContactData().withFirstname("Willy").withLastname("Wonka").withEmail("WWnuggets@mail.ru"));
+            app.contact().create(new ContactData()
+                    .withFirstname("Willy")
+                    .withLastname("Wonka")
+                    .withEmail("WWnuggets@mail.ru"));
         }
     }
     @Test (enabled = true)
     public void testContactModification() {
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         ContactData modifiedContact = before.iterator().next();
-        ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirstname(modifiedContact.getFirstname()).withLastname(modifiedContact.getLastname()).withEmail("somebody@mail.ru");
+        ContactData contact = new ContactData().withId(modifiedContact.getId())
+                .withFirstname(modifiedContact.getFirstname())
+                .withLastname(modifiedContact.getLastname())
+                .withEmail("somebody@mail.ru")
+                .withEmail2(modifiedContact.getEmail2())
+                .withEmail3(modifiedContact.getEmail3())
+                .withHomePhone(modifiedContact.getHomePhone())
+                .withMobilePhone(modifiedContact.getMobilePhone())
+                .withWorkPhone(modifiedContact.getWorkPhone());
+        app.goTo().homePage();
         app.contact().modify(contact);
         assertThat(app.contact().count(), equalTo(before.size()));
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
 
-        assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact.withEmail(null))));
+        assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
     }
 }
